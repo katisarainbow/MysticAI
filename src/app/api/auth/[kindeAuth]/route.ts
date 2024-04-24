a41c9af4ac1d65b4201ff70606e4ebc3fed47b76
katisarainbow <katietesara@gmail.com>
2024-04-24 16:41:23 +0200

    Fixing errors.

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,8 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET(request: NextRequest, { params }: any) {
-    const endpoint = params.kindeAuth;
-    return handleAuth(request, endpoint);
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const endpoint = req.query.kindeAuth as string;
+
+    return handleAuth(req, endpoint)(req, res);
 }
